a69ffff696e4a3decd8fe9c47773c1db40f300c9
andrew hulme <andrew_hulme04@hotmail.com>
2024-06-16 19:55:47 +1000

    Initial commit

---
+++ src/Greeting.jsx
@@ -0,0 +1,15 @@
+function Greeting() {
+  return (
+  <div>
+    <h1>Hello world!!</h1>;
+    <svg>
+      <circle cx="25" cy="25" r="20" stroke="green" strokeWidth="2" />
+    </svg>
+    <form>
+      <input type="text" />
+    </form>
+  </div>
+  );
+}
+
+export default Greeting;
