7976b7d47e11d81a5f6b7d2a8e638f4150801640
andrew hulme <andrew_hulme04@hotmail.com>
2024-06-17 21:16:44 +1000

    Added color state buttons

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import Animals from './Animals.jsx'
+import StateButton from './StateButton.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
     <Animals />
+    <StateButton />
   </React.StrictMode>,
 )
